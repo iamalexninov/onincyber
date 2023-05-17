e889e29a62c5823412753375016a775dc4279a62
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2023-05-17 16:24:51 +0300

    added typography file

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './sass/main.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolore quibusdam nostrum assumenda, perspiciatis, vero tempora quaerat libero quis culpa aperiam, dolorum molestias ad! Repudiandae, sint. A modi deserunt atque.</p>
-      <button>Click</button>
+      <h2>title</h2>
+      <p>paragraph</p>
     </div>
   );
 }
