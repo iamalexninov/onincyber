3ca4925a474a28877c683d0e0fe5b4e9dedcd185
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2023-05-17 15:50:38 +0300

    added breakpoints file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './sass/main.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Title</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolore quibusdam nostrum assumenda, perspiciatis, vero tempora quaerat libero quis culpa aperiam, dolorum molestias ad! Repudiandae, sint. A modi deserunt atque.</p>
+      <button>Click</button>
     </div>
   );
 }
